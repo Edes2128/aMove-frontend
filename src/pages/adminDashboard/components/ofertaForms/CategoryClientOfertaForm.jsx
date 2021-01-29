9d4bab53d9d7b4c6330c0ca6183b087bdd9fc432
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-01-29 15:57:12 +0100

    SAS

--- src/pages/adminDashboard/components/ofertaForms/CategoryClientOfertaForm.jsx
+++ src/pages/adminDashboard/components/ofertaForms/CategoryClientOfertaForm.jsx
@@ -1,15 +1,165 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import DepoContext from "../../../../context/depoContext/DepoContext";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
 
 export default function CategoryClientOfertaForm() {
-    return (
-        <>
+  useEffect(() => {
+    depoContext.getCategoryClients();
+    depoContext.getCategoryProducts();
+    depoContext.getAllProducts();
+  }, []);
 
-            <form className="oferta-categoryClient-form">
-                <div className="oferta-categoryClient-form-catKlient">
-                    
-                </div>
-            </form>
+  const depoContext = useContext(DepoContext);
+  const { categoryClients, produktet, categoryProducts } = depoContext;
+  const [kategoriKlientiZgjedhur, setKategoriKlientiZgjedhur] = useState({});
+  const [njesia, setNjesia] = useState("");
+  const [ulja, setUlja] = useState("");
+  const [dataFillimit, setDataFillimit] = useState("");
+  const [dataMbarimit, setDataMbarimit] = useState("");
+  const [kategoriteProdukteve, setKategoriteProdukteve] = useState([]);
+  const [produkteSpecifike, setProdukteSpecfike] = useState([]);
+  return (
+    <>
+      <form className="oferta-categoryClient-form">
+        <div className="oferta-categoryClient-form-catKlient">
+          <Autocomplete
+            id="combo-box-demo"
+            options={categoryClients}
+            getOptionLabel={(option) => option.name}
+            style={{ width: 200 }}
+            autoHighlight
+            onChange={(event, newValue) => {
+              setKategoriKlientiZgjedhur(newValue);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Kategoria Klientit"
+                variant="outlined"
+              />
+            )}
+          />
+          <TextField
+            style={{ width: "100px" }}
+            variant="outlined"
+            label="Ulja"
+            value={ulja}
+            onChange={(e) => setUlja(e.target.value)}
+          />
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="njesia-label">Njesia</InputLabel>
+            <Select
+              value={njesia}
+              onChange={(e) => setNjesia(e.target.value)}
+              style={{ width: "130px" }}
+              label="Status"
+              inputProps={{
+                name: "status",
+                id: "njesia-label",
+              }}
+            >
+              <MenuItem value={"Leke"}>Leke</MenuItem>
+              <MenuItem value={"%"}>%</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            type="date"
+            variant="outlined"
+            label="Data e fillimit"
+            onChange={(e) => setDataFillimit(e.target.value)}
+            value={dataFillimit}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            type="date"
+            variant="outlined"
+            label="Data e mbarimit"
+            value={dataMbarimit}
+            onChange={(e) => setDataMbarimit(e.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </div>
+        <div className="oferta-categoryClient-form-categoryProduct">
+          <Autocomplete
+            multiple
+            filterSelectedOptions
+            id="combo-box-demo"
+            options={categoryProducts}
+            getOptionLabel={(option) => option.name}
+            style={{ width: "100%" }}
+            autoHighlight
+            onChange={(event, newValue) => {
+              setKategoriKlientiZgjedhur(newValue);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Kategoria e Produktit"
+                variant="outlined"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            filterSelectedOptions
+            id="combo-box-demo"
+            options={produktet}
+            getOptionLabel={(option) => option.titulli}
+            style={{ width: "100%" }}
+            autoHighlight
+            onChange={(event, newValue) => {
+              setKategoriKlientiZgjedhur(newValue);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Produkt Specifik"
+                variant="outlined"
+              />
+            )}
+          />
+        </div>
+        <div className="oferta-categoryClient-form-setProduktesh">
+          <p>Set Produktesh</p>
 
-        </>
-    )
+          <div className="oferta-categoryClient-form-setProduktesh-item">
+            <Autocomplete
+              id="combo-box-demo"
+              options={produktet}
+              getOptionLabel={(option) => option.titulli}
+              style={{ width: "30%" }}
+              autoHighlight
+              // onChange={(event, newValue) => {
+              //   setProduktSpecifik(newValue);
+              // }}
+              renderInput={(params) => (
+                <TextField {...params} label="Produktet" variant="outlined" />
+              )}
+            />
+            <TextField variant="outlined" label="Bli" type={"number"} />
+            <p style={{ fontSize: "30px" }}>+</p>
+            <TextField variant="outlined" label="Perfito" type={"number"} />
+          </div>
+        </div>
+        <div className="oferta-categoryClient-form-buttons">
+          <Button color="primary" variant="outlined">
+            Ruaj
+          </Button>
+          <Button color="secondary" variant="contained">
+            Anullo
+          </Button>
+        </div>
+      </form>
+    </>
+  );
 }
--- src/pages/adminDashboard/components/ofertaForms/OfertaGrupForm.jsx
+++ src/pages/adminDashboard/components/ofertaForms/OfertaGrupForm.jsx
@@ -1,9 +1,231 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import DepoContext from "../../../../context/depoContext/DepoContext";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
 
 export default function OfertaGrupForm() {
-    return (
-        <div>
-            Oferta Grup
+  useEffect(() => {
+    depoContext.getCategoryClients();
+    depoContext.getCategoryProducts();
+    depoContext.getAllProducts();
+    depoContext.getAllClients();
+  }, []);
+  const depoContext = useContext(DepoContext);
+  const {
+    categoryClients,
+    produktet,
+    categoryProducts,
+    klientet,
+  } = depoContext;
+  const [catProductZgjedhur, setCatProductZgjedhur] = useState({});
+  const [catPrUlja, setCatPrUlja] = useState("");
+  const [catPrNjesia, setCatPrNjesia] = useState("");
+  const [catPrPeriudhaFillimit, setCatPrPeriudhaFillimit] = useState("");
+  const [catPrPeriudhaMbarimit, setCatPrPeriudhaMbarimit] = useState("");
+
+  const [produktSpecifikZgjedhur, setProduktSpecifikZgjedhur] = useState({});
+  const [prSpeUlja, setPrSpeUlja] = useState("");
+  const [prSpeNjesia, setPrSpeNjesia] = useState("");
+  const [prSpePeriudhaFillmit, setPrSpePeriudhaFillmit] = useState("");
+  const [prSpePeriudhaMbarimit, setPrSpePeriudhaMbarimit] = useState("");
+  return (
+    <>
+      <form className="oferta-grup-form">
+        <div className="oferta-grup-form-kategoriProdukti">
+          <Autocomplete
+            id="combo-box-demo"
+            options={categoryProducts}
+            getOptionLabel={(option) => option.name}
+            style={{ width: 200 }}
+            autoHighlight
+            onChange={(event, newValue) => {
+              setCatProductZgjedhur(newValue);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Kategoria Produktit"
+                variant="outlined"
+              />
+            )}
+          />
+          <TextField
+            style={{ width: "100px" }}
+            variant="outlined"
+            label="Ulja"
+            value={catPrUlja}
+            onChange={(e) => setCatPrUlja(e.target.value)}
+            type="number"
+          />
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="njesia-label">Njesia</InputLabel>
+            <Select
+              value={catPrNjesia}
+              onChange={(e) => setCatPrNjesia(e.target.value)}
+              style={{ width: "130px" }}
+              label="Status"
+              inputProps={{
+                name: "status",
+                id: "njesia-label",
+              }}
+            >
+              <MenuItem value={"Leke"}>Leke</MenuItem>
+              <MenuItem value={"%"}>%</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            type="date"
+            variant="outlined"
+            label="Data e fillimit"
+            onChange={(e) => setCatPrPeriudhaFillimit(e.target.value)}
+            value={catPrPeriudhaFillimit}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            type="date"
+            variant="outlined"
+            label="Data e mbarimit"
+            value={catPrPeriudhaMbarimit}
+            onChange={(e) => setCatPrPeriudhaMbarimit(e.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
         </div>
-    )
+        <div className="oferta-grup-form-produktSpecifik">
+          <Autocomplete
+            id="combo-box-demo"
+            options={produktet}
+            getOptionLabel={(option) => option.titulli}
+            style={{ width: 200 }}
+            autoHighlight
+            onChange={(event, newValue) => {
+              setProduktSpecifikZgjedhur(newValue);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Produkt Specifik"
+                variant="outlined"
+              />
+            )}
+          />
+          <TextField
+            style={{ width: "100px" }}
+            variant="outlined"
+            label="Ulja"
+            value={prSpeUlja}
+            onChange={(e) => setPrSpeUlja(e.target.value)}
+            type="number"
+          />
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="njesia-label">Njesia</InputLabel>
+            <Select
+              value={prSpeNjesia}
+              onChange={(e) => setPrSpeNjesia(e.target.value)}
+              style={{ width: "130px" }}
+              label="Status"
+              inputProps={{
+                name: "status",
+                id: "njesia-label",
+              }}
+            >
+              <MenuItem value={"Leke"}>Leke</MenuItem>
+              <MenuItem value={"%"}>%</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            type="date"
+            variant="outlined"
+            label="Data e fillimit"
+            onChange={(e) => setPrSpePeriudhaFillmit(e.target.value)}
+            value={prSpePeriudhaFillmit}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            type="date"
+            variant="outlined"
+            label="Data e mbarimit"
+            value={prSpePeriudhaMbarimit}
+            onChange={(e) => setPrSpePeriudhaMbarimit(e.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </div>
+        <div className="oferta-grup-form-kategoriKlienti">
+        <Autocomplete
+            id="combo-box-demo"
+            options={categoryClients}
+            getOptionLabel={(option) => option.name}
+            style={{ width: 200 }}
+            autoHighlight
+            onChange={(event, newValue) => {
+              setProduktSpecifikZgjedhur(newValue);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Kategori Klienti"
+                variant="outlined"
+              />
+            )}
+          />
+          <TextField
+            style={{ width: "100px" }}
+            variant="outlined"
+            label="Ulja"
+            value={prSpeUlja}
+            onChange={(e) => setPrSpeUlja(e.target.value)}
+            type="number"
+          />
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="njesia-label">Njesia</InputLabel>
+            <Select
+              value={prSpeNjesia}
+              onChange={(e) => setPrSpeNjesia(e.target.value)}
+              style={{ width: "130px" }}
+              label="Status"
+              inputProps={{
+                name: "status",
+                id: "njesia-label",
+              }}
+            >
+              <MenuItem value={"Leke"}>Leke</MenuItem>
+              <MenuItem value={"%"}>%</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            type="date"
+            variant="outlined"
+            label="Data e fillimit"
+            onChange={(e) => setPrSpePeriudhaFillmit(e.target.value)}
+            value={prSpePeriudhaFillmit}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            type="date"
+            variant="outlined"
+            label="Data e mbarimit"
+            value={prSpePeriudhaMbarimit}
+            onChange={(e) => setPrSpePeriudhaMbarimit(e.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </div>
+      </form>
+    </>
+  );
 }
