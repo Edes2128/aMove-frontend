000d3a4563c44207316447dd503125cfbb7d6ebb
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-02-16 11:22:28 +0100

    ASAS

--- src/pages/components/FormLogin.jsx
+++ src/pages/components/FormLogin.jsx
@@ -1,16 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { TextField, Button } from "@material-ui/core";
 import axios from "axios";
-
+import AlertContext from "../../context/alertContext/AlertContext";
+import Alerts from "./Alert";
 export default function FormLogin({ history }) {
+  const alertContext = useContext(AlertContext);
   const [email, setName] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  useEffect(() => {
-    if (!error == "") {
-      setTimeout(() => setError(""), 3000);
-    }
-  });
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -20,78 +16,81 @@ export default function FormLogin({ history }) {
         password,
       })
       .then((res) => {
-        console.log(res.data);
         if (res.data.status === 1 && res.data.role === "1") {
           localStorage.setItem("token", JSON.stringify(res.data.token));
           localStorage.setItem("auth", true);
-          history.push("/admin");
+          alertContext.setAlert(`${res.data.message}`, "success");
+          setTimeout(() => history.push("/admin"), 2000);
         } else if (res.data.status === 1 && res.data.role === "3") {
           localStorage.setItem("token", JSON.stringify(res.data.token));
           localStorage.setItem("auth", true);
           localStorage.setItem("id", JSON.stringify(res.data.id));
-          history.push("/klient/produktet");
+          alertContext.setAlert(`${res.data.message}`, "success");
+          setTimeout(() => history.push("/klient/produktet"), 2000);
         } else {
-          setError(res.data.message);
+          alertContext.setAlert(`${res.data.message}`, "error");
         }
       });
   };
 
   return (
-    <div className="App">
-      <form className="form-login-home" onSubmit={onSubmit}>
-        <div className="div-img-form-login-home">
-          <img
-            className="img-form-login-home"
-            src="move-logo.png"
-            alt="amove logo"
-          />
-        </div>
-        <p
-          style={{
-            fontSize: "17px",
-            color: "white",
-            textShadow: "0 5px 10px black",
-          }}
-        >
-          Sign in to continue
-        </p>
-        <div className="div-input-form-login-home">
-          <TextField
-            className="input-form-login-home"
-            type="email"
-            label="Email"
-            variant="outlined"
-            name="email"
-            required
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
+    <>
+      <Alerts />
+      <div className="App">
+        <form className="form-login-home" onSubmit={onSubmit}>
+          <div className="div-img-form-login-home">
+            <img
+              className="img-form-login-home"
+              src="move-logo.png"
+              alt="amove logo"
+            />
+          </div>
+          <p
+            style={{
+              fontSize: "17px",
+              color: "white",
+              textShadow: "0 5px 10px black",
+            }}
+          >
+            Sign in to continue
+          </p>
+          <div className="div-input-form-login-home">
+            <TextField
+              className="input-form-login-home"
+              type="email"
+              label="Email"
+              variant="outlined"
+              name="email"
+              required
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
 
-        <div className="div-input-form-login-home">
-          <TextField
-            className="input-form-login-home"
-            type="password"
-            label="Password"
-            variant="outlined"
-            name="passowrd"
-            style={{ borderColor: "white" }}
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <Button
-          className="submit-form-login-home"
-          type="submit"
-          style={{
-            backgroundColor: "#2a7ebf",
-            color: "white",
-            boxShadow: "0 4px 10px rgba(0,0,0,.3)",
-          }}
-        >
-          Submit
-        </Button>
-        <p className="error">{error}</p>
-      </form>
-    </div>
+          <div className="div-input-form-login-home">
+            <TextField
+              className="input-form-login-home"
+              type="password"
+              label="Password"
+              variant="outlined"
+              name="passowrd"
+              style={{ borderColor: "white" }}
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <Button
+            className="submit-form-login-home"
+            type="submit"
+            style={{
+              backgroundColor: "#2a7ebf",
+              color: "white",
+              boxShadow: "0 4px 10px rgba(0,0,0,.3)",
+            }}
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
+    </>
   );
 }
