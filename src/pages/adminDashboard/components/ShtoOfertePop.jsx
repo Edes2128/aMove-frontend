c1bab8d936b29507d85ff1837025bee369da04a4
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-03-01 11:46:47 +0100

    ASS

--- src/pages/adminDashboard/components/ShtoOfertePop.jsx
+++ src/pages/adminDashboard/components/ShtoOfertePop.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { CloseOutlined } from "@material-ui/icons";
+import TextField from '@material-ui/core/TextField';
+
 
 export default function ShtoOfertePop({ closePop }) {
 
-  return (
-    <div className="shto-oferte-pop">
-      <div className="shto-oferte-pop-opa" onClick={closePop}></div>
-      <div className="shto-oferte-pop-container">
-        <CloseOutlined
-          style={{
-            alignSelf: "flex-end",
-            marginRight: "10px",
-            marginTop: "5px",
-            fontSize: "30px",
-            cursor: "pointer",
-          }}
-          onClick={closePop}
-        />
+    return (
+        <div className="shto-oferte-pop" >
+            <div className="shto-oferte-pop-opa"
+                onClick={closePop} ></div>
+            <div className="shto-oferte-pop-container" >
+                <CloseOutlined style={
+                    {
+                        alignSelf: "flex-end",
+                        marginRight: "10px",
+                        marginTop: "5px",
+                        fontSize: "30px",
+                        cursor: "pointer",
+                    }
+                }
+                    onClick={closePop}
+                />
 
-                    
-
-      </div>
-    </div>
-  );
+                <div className="shto-oferte-pop-container-titulli" >
+                    <TextField style={
+                        { width: '90%' }}
+                        variant="outlined"
+                        label="Titulli" />
+                </div>
+            </div>
+        </div>
+    );
 }
